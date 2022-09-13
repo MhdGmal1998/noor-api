c4862559e2ece2cbab9aeee65e81f37a0ebb9291
Mohammed Gamal Al-homaidi <90135545+MhdGmal1998@users.noreply.github.com>
2022-09-13 16:41:38 +0300

    delete build app

--- dist/controllers/helloController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Hi = void 0;
-const Hi = (req, res) => {
-    res.send("Hi mom");
-};
-exports.Hi = Hi;
--- dist/util/generateNumber.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generateNumber = (digits) => {
-    const number = Math.floor(Math.random() * Math.pow(10, digits));
-    if (number.toString().length !== digits)
-        return generateNumber(digits);
-    return number;
-};
-exports.default = generateNumber;
